dfbabb65bfc615783cfdb0a345eae47ae0188c52
Exployer <pikusbgdan@gmail.com>
2025-08-09 21:28:55 +0300

    chore: MVVM

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -22,40 +22,5 @@ namespace CRM
         {
             InitializeComponent();
         }
-
-        private void Create_Database_Click(object sender, RoutedEventArgs e)
-        {
-            Create_Database_Window create_Database_Window = new Create_Database_Window();
-            create_Database_Window.Show();
-            this.Close();
-        }
-
-        private void Open_Database_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Documentation_Click(object sender, RoutedEventArgs e)
-        {
-            try {
-                Process.Start(new ProcessStartInfo { FileName = ".\\Documentation.pdf", UseShellExecute = true});
-            }
-            catch { MessageBox.Show("Throw new error while opening Documentation.pdf"); }
-        }
-
-        private void Close_MainWindow_Click(object sender, RoutedEventArgs e)
-        {
-            System.Environment.Exit(0);
-        }
-
-        private void Test_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                var process = Process.GetCurrentProcess();
-                MessageBox.Show($"id:{process.Id}, name:{process.ProcessName}, VirtualMemory: {process.VirtualMemorySize64}");
-            }
-            catch { MessageBox.Show("Test Error"); }
-        }
     }
 }
