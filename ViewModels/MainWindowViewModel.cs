7b44632bf8f1fe1b37e7e95ec2a289970da23198
Exployer <pikusbgdan@gmail.com>
2025-08-21 19:26:43 +0300

    feat: realize INotifyPropertyChanged for changing OpenDatabaseControl and CreateDatabaseControl, add event PropertyChangedEventHandler and 3 methods GoBackToMainWindowFromCreateControl,GoBackToMainWindowFromOpenControl, OnPropertyChange style: add 2 Bindings: GoToMainWindowFromOpenControl in OpenDatabaseControl.xaml and GoToMainWindowFromCreateControl in CreateDatabaseControl.xaml

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
@@ -15,17 +16,21 @@ using CRM.Templates;
 
 namespace CRM.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : INotifyPropertyChanged
     {
         public ICommand CreateDatabase { get; }
         public ICommand ExitSystem { get; }
         public ICommand OpenDatabase { get; }
         public ICommand OpenDocumentation { get; }
         public ICommand ConfirmDatabase { get; }
-        public string? DatabaseName { get; }
+        public ICommand GoToMainWindowFromCreateControl { get; }
+        public ICommand GoToMainWindowFromOpenControl { get; }
+        public string? DatabaseName { get; set; }
 
         public bool CreateDatabaseControlVisibility { get; set; }
         public bool OpenDatabaseControlVisibility { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public MainWindowViewModel()
         {
@@ -34,15 +39,19 @@ namespace CRM.ViewModels
             OpenDocumentation = new RelayCommand(Click => Documentation());
             ExitSystem = new RelayCommand(Click => System.Environment.Exit(0));
             ConfirmDatabase = new RelayCommand(Click => ConfirmDatabaseName());
+            GoToMainWindowFromCreateControl = new RelayCommand(Click => GoBackToMainWindowFromCreateControl());
+            GoToMainWindowFromOpenControl = new RelayCommand(Click => GoBackToMainWindowFromOpenControl());
 
         }
         private void CreateDB()
         {
-        CreateDatabaseControlVisibility = true;
+            CreateDatabaseControlVisibility = true;
+            OnPropertyChange(nameof(CreateDatabaseControlVisibility));
         }
         private void OpenDB()
         {
-            MessageBox.Show("OpenDatabase MessageBox");
+            OpenDatabaseControlVisibility = true;
+            OnPropertyChange(nameof(OpenDatabaseControlVisibility));
         }
         private void Documentation()
         {
@@ -58,5 +67,21 @@ namespace CRM.ViewModels
             }
 
         }
+        private void GoBackToMainWindowFromCreateControl()
+        {
+            CreateDatabaseControlVisibility = false;
+            OnPropertyChange(nameof(CreateDatabaseControlVisibility));
+        }
+        private void GoBackToMainWindowFromOpenControl()
+        {
+            OpenDatabaseControlVisibility = false;
+            OnPropertyChange(nameof(OpenDatabaseControlVisibility));
+        }
+
+        protected void OnPropertyChange(string TrueOrFalse)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(TrueOrFalse));
+        }
+
     }
 }   
