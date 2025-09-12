7f5388fb23c3ce5fbc620034801a133b3aa15001
Exployer <pikusbgdan@gmail.com>
2025-09-12 17:13:59 +0300

    feat: create bool ShowDialog in interface DialogInterface for opening Window from Control VM due to pattent mvvm; realize opening window, which is an instance of the interface in DialogService; realize DialogService in OrderControlViewModel.cs
    style: rewrote xaml markup OrderModalWindow.xaml

---
+++ Services/DialogService.cs
@@ -0,0 +1,17 @@
+﻿using CRM.Interfaces;
+using CRM.Views.ModalControls;
+using System;
+using System.Windows;
+using System.Windows.Automation.Peers;
+
+namespace CRM.Services
+{
+    public class DialogService : IDialogInterface
+    {
+        public bool? ShowDialog()
+        {
+            OrderModalWindow window = new();
+            return window.ShowDialog();
+        }
+    }
+}
--- ViewModels/OrderControlViewModel.cs
+++ ViewModels/OrderControlViewModel.cs
@@ -10,18 +10,21 @@ namespace CRM.ViewModels
 {
     public class OrderControlViewModel : NotifyPropertyChange
     {
+        private DialogService? service;
         public object? CurrentView { get; set; }
 
         public ICommand Exit {  get; set; }
         public ICommand AddOrderCommand { get; set; }
 
         public OrderControlViewModel()
-        {
+        {   
             Exit = new RelayCommand(Click => System.Environment.Exit(0));
             AddOrderCommand = new RelayCommand(Click => OpenOrderDialog());
         }
         private void OpenOrderDialog()
         {
+            service = new DialogService();
+            service.ShowDialog();
         }
     }
 }
