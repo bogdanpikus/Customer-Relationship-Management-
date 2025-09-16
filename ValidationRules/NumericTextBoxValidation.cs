bd7f34a34f872a6d005a96bf3f17dd6cb13afb80
Exployer <pikusbgdan@gmail.com>
2025-09-16 11:55:17 +0300

    style: update TextBox line 63-72 OrderModalWindow.xaml; chore: create folder ValidationRules + NumericTextBoxValidation.cs feat: realize NumericTextBoxValidation

---
+++ ValidationRules/NumericTextBoxValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CRM.ValidationRules
+{
+    public class NumericTextBoxValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(value is string v && v.Length > 12)
+            {
+                return new ValidationResult(false, "Значение толжно быть цифровым и не больше 12 символов");
+            }
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
--- ViewModels/OrderModalViewModel.cs
+++ ViewModels/OrderModalViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,6 @@ namespace CRM.ViewModels
         public OrderModalViewModel()
         {
         }
+     
     }
 }
