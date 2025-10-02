2a372a4c4503954ac270b7427809e53770c3bf69
Exployer <pikusbgdan@gmail.com>
2025-10-02 14:40:32 +0300

    chore: add package OxyPlot, add folder and Licence for OxyPlot
    feat: realize simple graff code for testing OxyPlot

--- ViewModels/AnalizeViewModel.cs
+++ ViewModels/AnalizeViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OxyPlot;
+using OxyPlot.Series;
 
 namespace CRM.ViewModels
 {
     public class AnalizeViewModel
     {
+        public PlotModel MyPlotModel { get; set; }
+        public AnalizeViewModel()
+        {
+            MyPlotModel = new PlotModel { Title = "Test" };
+            MyPlotModel.Series.Add(new LineSeries
+            {
+                ItemsSource = new List<DataPoint>
+            {
+                new DataPoint(0,0),
+                new DataPoint(1,2),
+                new DataPoint(2,4)
+            }
+            });
+        }
     }
 }
--- Views/UserControls/AnalizeControl.xaml.cs
+++ Views/UserControls/AnalizeControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRM.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace CRM.Views.UserControls
         public AnalizeControl()
         {
             InitializeComponent();
+            DataContext = new AnalizeViewModel();
         }
     }
 }
