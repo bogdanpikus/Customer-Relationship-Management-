ee7176f496a3631fe3b4aaab456f4f9a5e8eb38a
Exployer <pikusbgdan@gmail.com>
2025-11-16 10:39:48 +0200

    style: add datatarger in StorageControl.xaml;
    feat: realize method CreateStorageButton in StorageViewModel.cs

--- ViewModels/StorageViewModel.cs
+++ ViewModels/StorageViewModel.cs
@@ -1,15 +1,40 @@
-﻿using CRM.Models;
+﻿using CRM.Commands;
+using CRM.Models;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace CRM.ViewModels
 {
     public class StorageViewModel
     {
-        public ObservableCollection<Storages> Storages { get; } = new();
+        public ObservableCollection<Storages> StoragesCollection { get; } = new();
+
+        public string StorageName { get; set; }
+        public ICommand CreateStorage { get; }
+        public ICommand CreateGroup { get; }
+        public ICommand CreateProduct { get; }
+        public ICommand Editing {  get; }
+        public ICommand Delete { get; }
+
+        public ICommand OpenStorage { get; }
 
         public StorageViewModel() 
         {
+            CreateStorage = new RelayCommand(Click => CreateStorageButton());
+        }
+        private void CreateStorageButton()
+        {
+            StorageName = "Storage 1";
 
+            var storage = new Storages
+            {
+                Address = null,
+                Responsible = null,
+                Phone = null,
+                AmountGoodsInStorage = 0
+            };
+
+            StoragesCollection.Add(storage);
         }
     }
 }
